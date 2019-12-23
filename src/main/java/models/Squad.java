ad89e482771e980780f99f501ff0a3e4cee8c766
ADWESH <adweshshaddie@gmail.com>
2019-12-23 20:40:37 +0300

    Added getter methods for the private variables in the Squad class

--- src/main/java/models/Squad.java
+++ src/main/java/models/Squad.java
@@ -7,10 +7,28 @@ public class Squad {
     private String name;
     private int maxSize;
     private String cause;
+    private Hero newSquadMember;
 
-    public Squad(String name, int maxSize, String cause){
+    public Squad(String name, int maxSize, String cause, Hero newSquadMember){
         this.name = name;
         this.maxSize = maxSize;
         this.cause = cause;
+        this.newSquadMember = newSquadMember;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getMaxSize() {
+        return maxSize;
+    }
+
+    public String getCause() {
+        return cause;
+    }
+
+    public Hero getSquadMember() {
+        return newSquadMember;
     }
 }
